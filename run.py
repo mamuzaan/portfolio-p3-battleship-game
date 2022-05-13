88d03ea928aadc04aee8f496e4533320c183cc78
Yakub Ali <mamuzaan@yahoo.com>
2022-05-13 16:05:27 +0000

    Add import randint and sleep and comments it below

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+from time import sleep
+
+"""
+Import random go to generate random int in game.
+Import sleep from time, is used to add delay in the exacution of a program
+"""
