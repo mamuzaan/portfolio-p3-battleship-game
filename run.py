50c43804af76559ec3c3140d9c9b517dd02ba026
Yakub Ali <mamuzaan@yahoo.com>
2022-05-13 16:07:43 +0000

    Add greeting and instructions here

--- run.py
+++ run.py
@@ -5,3 +5,10 @@ from time import sleep
 Import random go to generate random int in game.
 Import sleep from time, is used to add delay in the exacution of a program
 """
+print("---------------------------------- \n"
+      "Wellcome to ULTIMATE BATTLESHIP! \n"
+      "Board size player can decide. \n"
+      "Top left corner is raw: 0, col: 0 \n"
+      "Grid size between 5 to 10 \n"
+      "---------------------------------- ")
+
